232a7c590d707882f7f735a84ca99366daf9374e
Aryan Kumar <aryankumar@Aryans-MacBook-Air.local>
2020-12-24 09:46:59 +0530

    Comments added to make it more readable

--- MyArrayList.java
+++ MyArrayList.java
@@ -5,17 +5,20 @@ import java.util.ArrayList;
 public class MyArrayList {
 
 	public static void main(String[] args) {
-		
+		//Created a new ArrayList
 		ArrayList<String> fruits = new ArrayList<>();
 		
+		//Added some elements to it
 		fruits.add("Apple");
 		fruits.add("Banana");
 		fruits.add("Orange");
 		
+		//Created another ArrayList 
 		ArrayList<String> vegetables = new ArrayList<>();
 		
 		vegetables.add("Tomato");
 		
+		//Using the addAll function
 		fruits.addAll(vegetables);
 		
 		String temp[] = new String[fruits.size()];
@@ -27,16 +30,20 @@ public class MyArrayList {
 		}
 		System.out.println("\n");
 		
+		//Using the get function
 		System.out.println(fruits.get(2)+"\n");
 		
+		//Using the remove function
 		fruits.remove(2);
 		
 		System.out.println(fruits+"\n");
 		
+		//Using the removeAll function
 		fruits.removeAll(vegetables);
 		
 		System.out.println(fruits+"\n");
 		
+		//Using the clear function
 		fruits.clear();
 		
 		System.out.println(fruits);
