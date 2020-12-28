584202c4cf4f0b78ee509a2ea54e2caf690ab528
Aryan Kumar <aryankumar@Aryans-MacBook-Air.local>
2020-12-28 09:11:51 +0530

    Modified fo the usage of stack

--- ImplementingLinkedList.java
+++ ImplementingLinkedList.java
@@ -4,7 +4,7 @@ public class ImplementingLinkedList<E> {
 	
 	Node<E> Head;
 	
-	void Add(E data) {
+	public void add(E data) {
 		
 		Node<E> toAdd = new Node<>(data);
 		
@@ -22,15 +22,55 @@ public class ImplementingLinkedList<E> {
 		temp.next = toAdd;
 	}
 	
+	public E removeLast() throws Exception {
+		
+		Node<E> temp = Head;
+		
+		if(temp == null) {
+			throw new Exception("Can not remove last element from empty linked list");
+		}
+		
+		if(temp.next == null) {
+			Node<E> toRemove = Head;
+			Head = null;
+			return (E) toRemove.data;
+		}
+		
+		while(temp.next.next != null) {
+			temp = temp.next;
+		}
+		
+		Node<E> toRemove = temp.next;
+		temp.next = null;
+		return (E) toRemove.data;
+	}
 	
+	public E getLast() throws Exception {
+		
+		Node<E> temp = Head;
+		
+		if(temp == null) {
+			throw new Exception("Can not peek last element from empty linked list");
+		}
+		
+		while(temp.next != null) {
+			temp = temp.next;
+		}
+		
+		return (E) temp.data;
+	}
 	
-	void Print() {
+	void print() {
 		Node<E> temp = Head;
 		while(temp != null) {
 			System.out.print(temp.data + " ");
 			temp = temp.next;
 		}
 		
+	}
+	
+	public boolean isEmpty() {
+		return Head == null;
 	}
 	
 	static class Node<E>{
